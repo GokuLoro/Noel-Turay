854667dc0805695e00ebe5bf40d7c6771e810775
GokuLoro <152174435+GokuLoro@users.noreply.github.com>
2024-10-09 12:16:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-let firstName 
-let lastName
+let firstName = document.getElementById("firstName");
+let lastName = document.getElementById("lastName");
+let myH1 = document.GetElementById("myH1");
+
+document.getElementById("mySubmit").onclick = function(){
+  myH1 = document.getElementById("firstName").value;
+  myH1 = document.getElementById("lastName").value;
+  document.getElementById("myH1").textContent = 'Hello ${firstName} ${lastName} and welcome to our website';
+
+  if("firstName" === "Noel"){
+    document.getElementById("label2").textContent = 'You are gay ${firstName} ${lastName};
+  }
+  else{
+    cument.getElementById("label2").textContent = 'You are not gay ${firstName} ${lastName};
+    
